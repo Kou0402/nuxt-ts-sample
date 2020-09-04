29056916576d89fae772411e39256d5aa4ef105b
kou0402 <kou.4869.biz@gmail.com>
2020-09-04 13:57:07 +0900

    feat: コメントコンポーネントを追加する

--- components/thread/MainThread.vue
+++ components/thread/MainThread.vue
@@ -1,5 +1,19 @@
 <template>
   <section class="p-6 border-2 container">
-    <h2 class="text-3xl">掲示板</h2>
+    <h2 class="mb-4 text-3xl">掲示板</h2>
+    <MainThreadComment class="mx-auto mb-4"></MainThreadComment>
+    <MainThreadComment class="mx-auto"></MainThreadComment>
   </section>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import MainThreadComment from '~/components/thread/MainThreadComment.vue'
+
+export default Vue.extend({
+  components: {
+    MainThreadComment,
+  },
+})
+</script>
---
+++ components/thread/MainThreadComment.vue
@@ -0,0 +1,9 @@
+<template>
+  <article class="w-5/6">
+    <h3 class="mb-1">
+      <span>スズキ</span>
+      <span>2020-01-01 10:10:10</span>
+    </h3>
+    <p class="px-4">コメントコメントコメントコメントコメントコメントコメント</p>
+  </article>
+</template>
