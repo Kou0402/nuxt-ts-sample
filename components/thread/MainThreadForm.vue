049d1e054c3113d1cff3fbbadc5025f2e5916949
kou0402 <kou.4869.biz@gmail.com>
2020-09-06 21:38:16 +0900

    feat: スレッドフォームにボタンを追加する

--- components/thread/MainThreadForm.vue
+++ components/thread/MainThreadForm.vue
@@ -5,19 +5,24 @@
       <input
         :value="name"
         type="text"
-        class="border-2 w-full"
+        class="border border-indigo w-full"
         @input="$emit('update:name', $event.target.value)"
       />
     </div>
-    <div class="flex">
+    <div class="flex mb-4">
       <label class="inline-block w-24">コメント</label>
       <input
         :value="comment"
         type="text"
-        class="border-2 w-full"
+        class="border border-indigo w-full"
         @input="$emit('update:comment', $event.target.value)"
       />
     </div>
+    <button
+      class="bg-blue hover:bg-blue-600 text-white font-bold py-2 px-4 border-b-2 border-black hover:border-blue-400 rounded"
+    >
+      投稿
+    </button>
   </section>
 </template>
 
