9d47cd1a3c081f3214bb3110982845ce26456bff
kou0402 <kou.4869.biz@gmail.com>
2020-09-06 13:13:44 +0900

    feat: スレッド投稿フォームを作成する

--- components/thread/MainThread.vue
+++ components/thread/MainThread.vue
@@ -1,7 +1,13 @@
 <template>
   <section class="p-6 border-2 container">
     <h2 class="mb-4 text-3xl">掲示板</h2>
-    <MainThreadComment class="mx-auto mb-4" :post="post"></MainThreadComment>
+    <MainThreadComment
+      v-for="post in posts"
+      :key="post.createdUserName"
+      :post="post"
+      class="mx-auto mb-6"
+    ></MainThreadComment>
+    <MainThreadForm :name.sync="name" :comment.sync="comment" class="mx-auto"></MainThreadForm>
   </section>
 </template>
 
@@ -9,21 +15,37 @@
 import Vue from 'vue'
 
 import MainThreadComment from '~/components/thread/MainThreadComment.vue'
+import MainThreadForm from '~/components/thread/MainThreadForm.vue'
 import { Post } from '~/models/post'
 
-const postSample: Post = {
-  createdUserName: 'スズキ',
-  createdAt: new Date(),
-  comment: 'コメントコメントコメントコメントコメントコメントコメント',
-}
+const postsSample: Post[] = [
+  {
+    createdUserName: 'スズキ',
+    createdAt: new Date(),
+    comment: 'コメントコメントコメントコメントコメントコメントコメント',
+  },
+  {
+    createdUserName: 'タナカ',
+    createdAt: new Date(),
+    comment: 'コメントコメントコメントコメントコメントコメントコメント',
+  },
+  {
+    createdUserName: 'コバヤシ',
+    createdAt: new Date(),
+    comment: 'コメントコメントコメントコメントコメントコメントコメント',
+  },
+]
 
 export default Vue.extend({
   components: {
     MainThreadComment,
+    MainThreadForm,
   },
   data() {
     return {
-      post: postSample,
+      posts: postsSample,
+      name: '',
+      comment: '',
     }
   },
 })
---
+++ components/thread/MainThreadForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="w-5/6">
+    <div class="flex mb-2">
+      <label class="inline-block w-24">名前</label>
+      <input
+        :value="name"
+        type="text"
+        @input="$emit('update:name', $event.target.value)"
+        class="border-2 w-full"
+      />
+    </div>
+    <div class="flex">
+      <label class="inline-block w-24">コメント</label>
+      <input
+        :value="comment"
+        type="text"
+        @input="$emit('update:comment', $event.target.value)"
+        class="border-2 w-full"
+      />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    comment: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+})
+</script>
