14d090f39e3e5c40441b1d1cf03485bb259d232f
kou0402 <kou.4869.biz@gmail.com>
2020-09-09 11:55:41 +0900

    feat: Moment.js を導入し、日付フォーマットを追加する

--- components/thread/MainThreadComment.vue
+++ components/thread/MainThreadComment.vue
@@ -2,7 +2,7 @@
   <article class="w-5/6">
     <h3 class="mb-1">
       <span>{{ post.createdUserName }}</span>
-      <span>{{ post.createdAt }}</span>
+      <span>{{ post.createdAt | moment }}</span>
     </h3>
     <p class="px-4">{{ post.comment }}</p>
   </article>
@@ -10,10 +10,16 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import moment from 'moment'
 
 import { Post } from '~/models/post'
 
 export default Vue.extend({
+  filters: {
+    moment(date: Date): string {
+      return moment(date).format('YYYY-MM-DD HH:mm:ss')
+    },
+  },
   props: {
     post: {
       type: Object as () => Post,
